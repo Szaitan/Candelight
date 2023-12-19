cb450eb461c6e0e2fa52c725b328229ef4351e0e
Michał <m.krepiniewicz@gmail.com>
2023-12-19 14:31:12 +0100

    Rev. 02.03 - Main_Page: Split sections 1 and 2, Footer HTML and CSS

--- candelight_website/views.py
+++ candelight_website/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, reverse
 from django.views import View
-
+import datetime
 # Create your views here.
+
+
+def get_date():
+    year = datetime.date.today().year
+    return year
 
 
 class IndexPageView(View):
     def get(self, request):
-        return render(request, "candelight_website/main_page.html")
+        year = get_date()
+        return render(request, "candelight_website/main_page.html", {
+            "year": year
+        })
