95aadba7000e0b4df5e2eecbe5244850a7b48b69
Michał <m.krepiniewicz@gmail.com>
2023-12-23 14:38:41 +0100

    Rev. 06.03 - Contact Page: HTML, CSS, form, SMTP

--- candelight_website/views.py
+++ candelight_website/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, reverse
+from django.shortcuts import render, reverse, redirect
 from django.views import View
 import datetime
 from candelight_website.models import RealisationsProject, RealisationsType
 from django.http import JsonResponse
 from candelight_website.forms import ContactMeForm
 import os
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 # Create your views here.
 
 
@@ -72,9 +75,26 @@ class ContactPageView(View):
         def post(self, request):
             form = ContactMeForm(request.POST)
             if form.is_valid():
-                return render(request, "candelight_website/contact_page.html", {
-                    "form": form
-                })
+                clean_data = form.cleaned_data
+                with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+                    connection.starttls()
+                    connection.login(user=os.environ.get("michal_mail"),
+                                     password=os.environ.get("google_app_access_password"))
+
+                    name = clean_data["name"]
+                    last_name = clean_data["last_name"]
+                    phone_number = clean_data["phone_number"]
+                    email = clean_data["email"]
+                    message = clean_data["message"]
+
+                    msg = MIMEText(
+                        f"Name: {name}\nLast Name: {last_name}\nPhone Number: {phone_number}\nEmail: {email}\n\n{message}",
+                        'plain', 'utf-8')
+                    msg['Subject'] = Header(name, 'utf-8').encode()
+                    connection.sendmail(from_addr=os.environ.get("michal_mail"), to_addrs=os.environ.get("michal_mail"),
+                                        msg=msg.as_string())
+
+                return redirect(reverse("contact_page"))
             else:
                 return render(request, "candelight_website/contact_page.html", {
                     "form": form
