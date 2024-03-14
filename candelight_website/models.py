9fa060071859f4c3c2b82c7f90a4e0c0ebe02a83
michal.krepiniewicz <m.krepiniewicz@gmail.com>
2024-03-14 13:04:55 +0100

    Kutno - Implementation of new way of filtering data, new form of data send via JS - rev.03

--- candelight_website/models.py
+++ candelight_website/models.py
@@ -18,6 +18,7 @@ class RealisationsType(models.Model):
         super(RealisationsType, self).delete(*args, **kwargs)
 
 
+
 class RealisationsProject(models.Model):
     object = models.CharField(max_length=75)
     arrangement = models.CharField(max_length=75)
